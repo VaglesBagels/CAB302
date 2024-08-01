48129f9ac97c01d36cb6e11cc6df193125625fa5
VaglesBagels <104954408+VaglesBagels@users.noreply.github.com>
2024-08-01 19:33:47 +1000

    troubleshooting

--- Prac 2/Prac 2/src/Main.java
+++ Prac 2/Prac 2/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static ArrayList<User> users = new ArrayList<>();
 
     // Mock authentication service that always returns the first user when log in, and does nothing when sign up
-    private static new AuthenticationService(users) = new IAuthenticationService() {
+    private static IAuthenticationService authService = new IAuthenticationService() {
         @Override
         public User signUp(String username, String password) {
             return null;
